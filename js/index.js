165736f530d22ce8f8439253686874b5166e758e
Andrés Espejo <118082536+Percival-Mirror@users.noreply.github.com>
2024-04-18 21:46:07 -0400

    Add files via upload

--- js/index.js
+++ js/index.js
@@ -12,6 +12,22 @@ let index = 1;
 
 mainArrow.addEventListener('click', mainSlide);
 
+function preload(arrayOfImages) {
+    $(arrayOfImages).each(function(){
+        $('<img/>')[0].src = this;
+    });
+}
+
+preload([
+    'media/bg/slide-1.jpg',
+    'media/bg/slide-2.jpg',
+    'media/bg/slide-3.jpg'
+]);
+
+$(function(){
+    $('body').show();
+});
+
 function mainSlide(){
 	if (index >= mainPages.length) {
 		index = 1;
